cb393a16cc2806102b7b666fbb7304bbae91fee7
Tim Wheeler <tim@timwheeler.com>
2019-05-20 21:06:11 -0400

    working video info tool

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const electron = require('electron')
+const ffmpeg = require('fluent-ffmpeg')
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 const { MetaData } = require('fluent-ffmpeg')
 
+let mainWindow;
+
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         }
     })
     mainWindow.loadURL(`file://${ __dirname }/index.html`)
+})
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        if (err) {
+            console.log(`Error: ${err}`)
+        } else {
+            const { duration } = metadata.format;
+            mainWindow.webContents.send('video:metadata', duration)
+            // console.log(`Video duration is: ${metadata.format.duration} seconds`)
+        }
+    })
 })
 
 
